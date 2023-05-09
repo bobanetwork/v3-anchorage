786b3f3abe635430d9c709adf055b0139778bdda
Michael Montour <mmontour@enya.ai>
2023-05-09 11:25:12 -0700

    WIP - broken  Changes to be committed: 	modified:   op-bindings/bindings/bobahchelper.go 	modified:   op-bindings/bindings/bobahchelper_more.go 	modified:   op-erigon 	modified:   packages/contracts-bedrock/contracts/boba/BobaHCHelper.sol

--- packages/contracts-bedrock/contracts/boba/BobaHCHelper.sol
+++ packages/contracts-bedrock/contracts/boba/BobaHCHelper.sol
@@ -2,96 +2,91 @@
 pragma solidity ^0.8.15;
 
 //import '@openzeppelin/contracts/access/Ownable.sol';
+//import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+//import "@openzeppelin/contracts/utils/introspection/ERC165Checker.sol";
 
 // import "./ITuringHelper.sol";
 import { Burn } from "../libraries/Burn.sol";
 
 contract BobaHCHelper /*is Ownable*/ {
+    //using SafeMath for uint256;
+    using SafeERC20 for IERC20;
 
   address immutable HelperAddr = 0x42000000000000000000000000000000000000Fd;
   address immutable OffchainCaller = 0xdEAddEadDeaDDEaDDeadDeAddeadDEaddeaD9901;
+  address immutable hcToken = 0x42000000000000000000000000000000000000FE;	// Boba token
+ 
+  // Cost (in hcToken) for various operations
+  uint256 constant RegCost = 100;  // Register an endpoint
+  uint256 constant CallCost = 50;  // Call off-chain
+  uint256 constant RNGCost = 30;   // Use a random-number function
 
-  event OffchainRandom(uint version, uint256 random);
-  event DBG(bytes b);
+  // Collected balance
+  uint256 public ownerRevenue;
 
+  mapping(address => uint256) public prepaidCredit;
+  mapping(address => uint256) public pendingCharge;
+
+  struct EndpointEntry {
+  	address Owner;
+	//string URL;
+	//uint256 CreditBalance;
+	mapping (address => bool) PermittedCallers;
+  }
+
+  mapping (bytes32 => EndpointEntry) public Endpoints;
   mapping(bytes32 => uint32) OffchainResponseCode;
   mapping(bytes32 => bytes)  OffchainResponseData;
 
-/*
-  // This protects your own credits for this helper contract
-  mapping(address => bool) public permittedCaller;
 
-  event AddPermittedCaller(address _callerAddress);
-  event RemovePermittedCaller(address _callerAddress);
-  event CheckPermittedCaller(address _callerAddress, bool permitted);
-  event OffchainResponse(uint version, bytes responseData);
-  event Offchain42(uint version, uint256 random);
-
-
-  modifier onlyPermittedCaller() {
-    require(
-      permittedCaller[msg.sender],
-      'Invalid Caller Address'
-    );
-    _;
-  }
-*/
+  // events 
+  event OffchainRandom(uint version, uint256 random);
+  event DBG(bytes b);
+  event AddBalanceTo(address, bytes32, uint256);
+  //event WithdrawRevenue(address sender, uint256 withdrawAmount);
 
 
   constructor () {
    }
 
   // -------------------------------------------------------------------------
-  
-  /* Internal functions which provide the interface to core/vm/hybrid_compute.go. GetResponse()
-    is called from other functions in this contract while PutResponse() and ExpireResponse() are
-    called from Offchain transactions inserted by the sequencer node.
-    
-    The first time that GetResponse() is called, it will revert on a "missing cache entry". On a
-    successful interaction, the offchain interactions will take place in the background and then
-    the user transaction will be re-run. This second time it will find a cache entry and return
-    the result. On a failure, the user transaction will be re-run a second time without any special
-    logic, so that the "missing cache entry" is passed back to the caller.
-    
-    TODO - This could be re-written to provide an in-band error response by adding a success/failure
-    flag to the cache entry.
+
+
+  /* Internal functions to compute a cache key and retrieve a result. Each
+     method computes a cache key based on its parameters, then calls a common
+     function to retrieve the response. If that reverts then the sequencer
+     will extract the request parameters and perform the corresponding
+     operation, writing the result into the cache and then re-running the
+     original transaction which will now find the result it is expecting.
+
+     ResponseCode is a packed structure of:
+       - Version (internal consistency check)
+       - User result code (0 = success, other = error)
+       - Extra L2 Gas to burn
+       - Boba token cost
+     The L2 gas is provided as a mechanism to offset L1 storage costs. If the
+     L1 cost is applied directly then this field should be set to 0.
   */
   
-  // Try to get a previously-populated response
-  function GetResponse(uint32 _rType, address _caller, string memory _url, bytes memory _payload)
-    public returns (bool, bytes memory) {
-    bool success;
-    bytes memory responseData;
-
-    // This function may only be called from the Helper contract
-    require (msg.sender == address(this), "Invalid GetResponse() caller");
-
-    bytes32 cacheKey = keccak256(abi.encodePacked(_rType, _caller, _url,  _payload));
-
-    uint32 code = OffchainResponseCode[cacheKey];
+  function GetResponse(bytes32 _cacheKey) private returns (bool, bytes memory) {
   
-    responseData = OffchainResponseData[cacheKey];
     
-    if (code == 0) {
-      // This specific revert string triggers the offchain mechanism. Do not change.
-      require(responseData.length > 0, "GetResponse: Missing cache entry");
-
-      success = true;
-
-      // We burn additional L2 gas here to reflect the cost of storing the Response data in the L1 rollup.
-      // TODO - calculate (L1_Gas_per_byte * Length) * (L1_Gas_Price / L2_Gas_Price). 
-      uint256 gasToBurn = responseData.length;
-      Burn.gas (gasToBurn * 5000);
+    bytes memory cachedResponse = OffchainResponse[_cacheKey];
+    require(cachedResponse.length > 0, "GetResponse: Missing cache entry");
+    uint256 responseInfo;
+    uint32 responseCode;
+    bytes memory responseData;
+    (responseInfo, responseData) = abi.decode(cachedResponse,(uint256,bytes));
+    
+    if (responseCode == 0) {
+      return (true, responseData);
     } else {
-      string memory errMsg = "HybridCompute Error";  // TODO - look up a string based on the code.
-      responseData = abi.encodeWithSignature("Error(string)", errMsg);
+      // Error code lookup?
+      return (false, responseData);
     }
-
-    delete(OffchainResponseData[cacheKey]);
-    delete(OffchainResponseCode[cacheKey]);
-    return (success, responseData);
   }
-  
+
   // Populate the cache ahead of an anticipated GetResponse(), charging Boba tokens.
   function PutResponse(bytes32 cacheKey, uint32 _code, bytes memory _payload) public {
     // Reserved address for Offchain transactions (core/types/offchain_tx.go)
@@ -121,6 +116,55 @@ contract BobaHCHelper /*is Ownable*/ {
     delete(OffchainResponseData[cacheKey]);
   }
 
+
+  // ===================================================================
+
+  /* Internal functions which provide the interface to core/vm/hybrid_compute.go. GetResponse()
+    is called from other functions in this contract while PutResponse() and ExpireResponse() are
+    called from Offchain transactions inserted by the sequencer node.
+    
+    The first time that GetResponse() is called, it will revert on a "missing cache entry". On a
+    successful interaction, the offchain interactions will take place in the background and then
+    the user transaction will be re-run. This second time it will find a cache entry and return
+    the result. On a failure, the user transaction will be re-run a second time without any special
+    logic, so that the "missing cache entry" is passed back to the caller.
+  */
+  
+  // Try to get a previously-populated response
+  function GetResponseV3(address _caller, string memory _url, bytes memory _payload)
+    public returns (bool, bytes memory) {
+    bool success;
+    bytes memory responseData;
+
+    require (msg.sender == address(this), "Invalid GetResponse() caller");
+
+    bytes32 cacheKey = keccak256(abi.encodePacked(_caller, _url,  _payload));
+
+    uint32 code = OffchainResponseCode[cacheKey];
+    responseData = OffchainResponseData[cacheKey];
+
+    if (code == 0) {
+      // This specific revert string triggers the offchain mechanism. Do not change.
+      require(responseData.length > 0, "GetResponse: Missing cache entry");
+      success = true;
+
+      // Placeholder to account for extra L1 storage costs
+      // TODO - calculate (L1_Gas_per_byte * Length) * (L1_Gas_Price / L2_Gas_Price),
+      // and merge with other L1 Fee logic
+      uint256 gasToBurn = responseData.length;
+      Burn.gas (gasToBurn * 5000);
+    } else {
+      string memory errMsg = "HybridCompute Error";  // TODO - look up a string based on the code.
+      responseData = abi.encodeWithSignature("Error(string)", errMsg);
+    }
+
+    delete(OffchainResponseData[cacheKey]);
+    delete(OffchainResponseCode[cacheKey]);
+    return (success, responseData);
+  }
+
+
+
   // -------------------------------------------------------------------------
 
   /* These functions are called to manage endpoints */
@@ -143,43 +187,45 @@ contract BobaHCHelper /*is Ownable*/ {
   // The address which calls RegisterEndpoint() is registered as its owner, 
   // and only that owner will be permitted to unregister it later.
   
-  struct EndpointEntry {
-  	address Owner;
-	string URL;
-	mapping (address => bool) PermittedCallers;
-  }
-
-  mapping (bytes32 => EndpointEntry) public Endpoints;
   
   // Emitted on every attempt to register an endpoint, including failures
   // which progressed far enough to send an offchain request. Boba credits
   // are consumed per attempt, not per success.
   event EndpointRegistered(bool Success, bytes32 EK, address Owner, string URL);
 
-  uint256 RegCredits = 100;
-
-  function RegisterEndpoint(string memory url, uint256 credits, bytes32 auth)
+ function RegisterEndpoint(string memory url, uint256 credits, bytes32 auth)
     public returns (bool) {
     
     BobaHCHelper Self = BobaHCHelper(HelperAddr);
     bytes32 EK = keccak256(abi.encodePacked(url));
     bool success;
-
-    require(credits >= RegCredits, "Insufficient registration credit");
-    // FIXME - process the payment and require success
     
+    require(credits >= RegCost, "Insufficient registration credit");
+    // Transfer token to this contract, crediting any leftover balance to the endpoint
+
     // Future registration protocols may perform a 2-way exchange, but
     // for now we only care about authenticating the endpoint to the Helper contract.
     bytes memory request = abi.encodeWithSignature("RegisterV1()");
     bytes memory response; 
 
     (success, response) = Self.GetResponse(1, address(this), url, request);
-    
+
     if (success && response.length == 32 && keccak256(response) == auth) {
       // Success
+
+      address oldOwner = Endpoints[EK].Owner;
+      uint256 oldBalance = Endpoints[EK].CreditBalance;
       
-      // FIXME - if overwriting an old entry, emit an Unregistration and deal with credits.
-      
+      // Process payment from new owner
+      IERC20(hcToken).safeTransferFrom(msg.sender, address(this), credits);
+      Endpoints[EK].CreditBalance += (credits - RegCost);
+      ownerRevenue += RegCost;
+
+      // Refund previous owner in case of overwrite
+      if (oldOwner != address(0) && oldBalance > 0) {
+        IERC20(hcToken).safeTransfer(oldOwner, oldBalance);
+      }
+
       Endpoints[EK].Owner = msg.sender;
       Endpoints[EK].URL = url;
       success = true;
@@ -193,10 +239,12 @@ contract BobaHCHelper /*is Ownable*/ {
 
   function UnregisterEndpoint(bytes32 EK) public {
     require(Endpoints[EK].Owner == msg.sender, "Only owner may unregister");
-    // FIXME refund any leftover credits to Owner
+    
+    uint256 leftoverBalance = Endpoints[EK].CreditBalance;
     delete(Endpoints[EK]);
+    IERC20(hcToken).safeTransfer(msg.sender, leftoverBalance);
   }
-  
+
   // Add a user contract's address to the list of permitted callers for the 
   // specified previously-registered endpoint.
   // TODO - credit limits?
@@ -220,6 +268,32 @@ contract BobaHCHelper /*is Ownable*/ {
     return(Endpoints[_EK].PermittedCallers[_callerAddress]);
   }
 
+  // Replaces BobaTuringCredit functionality
+  function addBalanceTo(bytes32 _EK, uint256 _addBalanceAmount)
+      public
+     // onlyInitialized
+  {
+      require(Endpoints[_EK].Owner != address(0), "Endpoint is not registered");
+      require(Endpoints[_EK].Owner == msg.sender, "Not the Endpoint owner");
+
+      require(_addBalanceAmount != 0, "Invalid amount");
+      /*
+      require(Address.isContract(_helperContractAddress), "Address is EOA");
+      require(
+  	  ERC165Checker.supportsInterface(_helperContractAddress, 0x2f7adf43),
+  	  "Invalid Helper Contract"
+      );
+      */
+
+      // Transfer token to this contract
+      IERC20(hcToken).safeTransferFrom(msg.sender, address(this), _addBalanceAmount);
+
+      Endpoints[_EK].CreditBalance += _addBalanceAmount;
+
+      emit AddBalanceTo(msg.sender, _EK, _addBalanceAmount);
+  } 
+  
+
   // -------------------------------------------------------------------------
 
   /* The following functions are called by users */
@@ -230,6 +304,12 @@ contract BobaHCHelper /*is Ownable*/ {
     require(bytes(URL).length > 0, "Endpoint not registered");
 
     require(Endpoints[EK].PermittedCallers[msg.sender], "Caller is not permitted");
+    
+    require(Endpoints[EK].CreditBalance >= CallCost, "Insufficient credit balance");
+    
+    Endpoints[EK].CreditBalance -= CallCost;
+    ownerRevenue += CallCost;
+
     bool success;
     bytes memory returnData;
     
@@ -242,6 +322,16 @@ contract BobaHCHelper /*is Ownable*/ {
     bool success;
     bytes memory response;
     
+    // This demonstrates a different option for billing. As random numbers are a
+    // built-in function, there is no Endpoint and therefore no credit balance to use.
+    // Instead we process a payment directly from the caller.
+    //
+    // An alternative would be to define non-URL Endpoints and give them credits.
+    
+    require(IERC20(hcToken).allowance(msg.sender, address(this)) >= RNGCost, "Insufficient credit authorization");
+    IERC20(hcToken).safeTransferFrom(msg.sender, address(this), RNGCost);
+    ownerRevenue += RNGCost;
+
     (success, response) = Self.GetResponse(65537, msg.sender, "", "");
     require(success, "Operation failed");
     require(response.length == 32, "Bad response length");
@@ -251,6 +341,23 @@ contract BobaHCHelper /*is Ownable*/ {
     return result;
   }
 
+  // Secure sequential RNG. Clients contribute their own randomness which is XOR-ed with
+  // a server-side secret to generate the final answer, ensuring that neither side is able
+  // to force a desired outcome. Each side provides a hash committment of its secret value
+  // in the first transaction, revealing the secret in a later block (TBD whether it's
+  // the next block or whether a greater confirmation depth will be required).
+  //
+  // "session" is an arbitrary identifier allowing a client to manage multiple ongoing
+  // random sequences. The server combines this key with msg.sender to generate its internal
+  // key for maaintaining state. Server-side states will eventually expire and may be lost
+  // for other reasons. Callers must implement error handling.
+  // 
+  // A "nextHash" of bytes32(0) will terminate a sequence, freeing server state.
+  // A "myNum" of 0 with a non-zero nextHash initiates a new session, overwriting 
+  // any previous server state. 
+  
+  function NextRandom(bytes32 session, bytes32 nextHash, uint256 myNum) public returns (bytes32, uint256) {
+  }
   // -------------------------------------------------------------------------
 
 
@@ -376,4 +483,22 @@ contract BobaHCHelper /*is Ownable*/ {
         return _interfaceId == firstSupportedInterface || _interfaceId == secondSupportedInterface;
     }
 */
+/*
+  event AddPermittedCaller(address _callerAddress);
+  event RemovePermittedCaller(address _callerAddress);
+  event CheckPermittedCaller(address _callerAddress, bool permitted);
+  event OffchainResponse(uint version, bytes responseData);
+  event Offchain42(uint version, uint256 random);
+
+
+  modifier onlyPermittedCaller() {
+    require(
+      permittedCaller[msg.sender],
+      'Invalid Caller Address'
+    );
+    _;
+  }
+*/
+
+
 }
