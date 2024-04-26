90175ffe6115b6c4994ee41fc555ec02ce5977e2
cby3149 <cby3149@gmail.com>
2024-04-26 16:08:00 -0700

    Fix image build

--- packages/boba/account-abstraction/scripts/prepack-contracts-package.sh
+++ packages/boba/account-abstraction/scripts/prepack-contracts-package.sh
@@ -5,6 +5,7 @@ cd "$(dirname "$0")"/..
 pwd
 
 mkdir -p artifacts_clean
+# shellcheck disable=SC2046
 cp $(find  ./artifacts/contracts -type f | grep -v -E 'Test|dbg|gnosis|bls|IOracle') artifacts_clean/
 typechain --target ethers-v5 --out-dir types artifacts_clean/**
 rm -r artifacts_clean
