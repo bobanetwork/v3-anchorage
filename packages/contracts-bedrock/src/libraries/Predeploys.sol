5d563c1217b354ec0826f97dbe2f0b75cab838ca
cby3149 <cby3149@gmail.com>
2024-08-16 10:10:17 -0700

    Fix contract tests

--- packages/contracts-bedrock/src/libraries/Predeploys.sol
+++ packages/contracts-bedrock/src/libraries/Predeploys.sol
@@ -132,7 +132,7 @@ library Predeploys {
 
     /// @notice Returns true if the predeploy is not proxied.
     function notProxied(address _addr) internal pure returns (bool) {
-        return _addr == GOVERNANCE_TOKEN || _addr == WETH || _addr == L2_BOBA;
+        return _addr == GOVERNANCE_TOKEN || _addr == WETH || _addr == LEGACY_ERC20_ETH || _addr == L2_BOBA;
     }
 
     /// @notice Returns true if the address is a defined predeploy that is embedded into new OP-Stack chains.
