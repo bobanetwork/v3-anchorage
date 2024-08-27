080fed0edda1c5c3b58cb6faa0a7a7b04102acb1
cby3149 <cby3149@gmail.com>
2024-08-27 13:55:54 -0700

    Fix unit tests

--- packages/contracts-bedrock/scripts/deploy/FPUpgrade.s.sol
+++ packages/contracts-bedrock/scripts/deploy/FPUpgrade.s.sol
@@ -54,10 +54,7 @@ contract Deploy is Deployer {
     /// @notice read proxyd address from the hardhat deployment files
     function readProxyAddress(string memory _contractName) internal view returns (address _proxyAddress) {
         string memory _deploymentPath = vm.envOr("DEPLOYMENT_PATH", string(""));
-        require(
-            bytes(_deploymentPath).length > 0,
-            "Deploy: must set DEPLOYMENT_PATH to deployment files"
-        );
+        require(bytes(_deploymentPath).length > 0, "Deploy: must set DEPLOYMENT_PATH to deployment files");
         string memory _contractJson = vm.readFile(_deploymentPath);
         bytes memory _contractAddress = stdJson.parseRaw(_contractJson, string(abi.encodePacked(".", _contractName)));
         _proxyAddress = bytesToAddress(_contractAddress);
