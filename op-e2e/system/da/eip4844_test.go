f74d8ffc8c82aca1299aae9876c2aba270fbd117
cby3149 <cby3149@gmail.com>
2025-02-14 22:00:48 -0800

    Increase waiting time

--- op-e2e/system/da/eip4844_test.go
+++ op-e2e/system/da/eip4844_test.go
@@ -361,7 +361,7 @@ func TestBatcherAutoDA(t *testing.T) {
 	require.Less(t, feeRatio, 16.0, "expected fee ratio to be less than 16 (blobspace should be cheaper, even without Pectra)")
 
 	// Now wait for batcher to have switched to blob txs.
-	requireEventualBatcherTxType(types.BlobTxType, 15*time.Second, false)
+	requireEventualBatcherTxType(types.BlobTxType, 30*time.Second, false)
 }
 
 func u64Ptr(v uint64) *uint64 {
